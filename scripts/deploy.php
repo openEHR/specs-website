89479531b3bf53736b858c8356f4128b6950bdd9
wolandscat <thomas.beale@openehr.org>
2019-01-26 14:58:22 +0000

    Fix some formatting.

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -11,35 +11,34 @@
         'git pull',
         'git log -n 3',
         'git --work-tree=%work_tree% checkout -f ',
-    	'git status',
+        'git status',
         'chmod ug+x %work_tree%/scripts/*.sh'
     );
 
     // Run the commands for output
     $output = '';
 
-	// get directory like /var/www/vhosts/openehr.org/specifications.openehr.org
+    // get directory like /var/www/vhosts/openehr.org/specifications.openehr.org
     $work_tree = dirname (getcwd());
 
-	// get site_name like specifications.openehr.org
+    // get site_name like specifications.openehr.org
     $site_name = basename ($work_tree);
 
-	// determine repo location as /var/www/git/specifications.openehr.org
+    // determine repo location as /var/www/git/specifications.openehr.org
     $repodir = "/var/www/git/" . $site_name;
     chdir ($repodir);
 
     $output .= "chdir (" . $repodir . ")\n";
 
     foreach ($commands AS $command){
-
         $command = str_replace ("%work_tree%", $work_tree, $command);
 
         // Run it
-		$cmd_output = '';
-		exec ($command, $cmd_output);
+        $cmd_output = '';
+        exec ($command, $cmd_output);
 
-        // Output
-		$output .= htmlentities('----------- execute ' . $command . ' --------------' . PHP_EOL . implode(PHP_EOL, $cmd_output) . PHP_EOL);
+        // Gather output
+        $output .= htmlentities('----------- execute ' . $command . ' --------------' . PHP_EOL . implode(PHP_EOL, $cmd_output) . PHP_EOL);
     }
 ?>
 <!DOCTYPE HTML>
