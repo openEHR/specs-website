5dc302ee5b4e8a6f5bb8e1e598729eb81847e360
wolandscat <thomas.beale@openehr.org>
2020-03-02 18:20:11 +0000

    More menu adjustments

--- menu/topmenu.php
+++ menu/topmenu.php
@@ -3,14 +3,14 @@
             <ul>
                 <li><a href="/latest_releases" <?php if($pageOn == 'latest_releases.php'){?> class="active"<?php }?>>Latest Releases</a></li>
                 <li><a href="/historical_releases" <?php if($pageOn == 'historical_releases.php'){?> class="active"<?php }?>>Historical Releases</a></li>
-                <li></li>
+                <li> </li>
                 <li><a href="https://openehr.atlassian.net/secure/Dashboard.jspa?selectPageId=10190" target="_blank">Issue Trackers</a></li>
                 <li><a href="https://www.openehr.org/programs/specification/governance" target="_blank">Governance</a></li>
-                <li></li>
+                <li> </li>
                 <li><a href="https://www.openehr.org/governance/intellectual_property" target="_blank">IP and Licensing</a></li>
                 <li><a href="https://discourse.openehr.org/c/specifications" target="_blank">Discourse Forum</a></li>
                 <li><a href="/wiki/display/spec/Specifications+Home" target="_blank">Wiki</a></li>
-                <li></li>
+                <li> </li>
                 <li><a href="/releases/BASE/latest/architecture_overview.html" target="_blank">Architecture Overview</a></li>
                 <li><a href="/releases/AM/latest/Overview.html" target="_blank">Archetype Technology</a></li>
                 <li><a href="https://openehr.atlassian.net/wiki/spaces/spec/pages/357957633/Services+Landscape+for+e-Health" target="_blank">Services Landscape</a></li>
@@ -46,8 +46,8 @@
                 <li><a href="/releases/SM/latest/index" target="_blank">SM</a></li>
                 <li><a href="/releases/SM/latest/openehr_platform.html" target="_blank">Platform Services</a></li>
                 <li><a href="/releases/SM/latest/simplified_im_b.html" target="_blank">SIM-B</a></li>
-                <li><a href="/components/SM/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECSM?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
+                <li><a href="/components/SM/open_issues" target="_blank">SM PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECSM?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">SM CRs</a></li>
             </ul>
         </li>
 
@@ -56,16 +56,16 @@
                 <li><a href="/releases/CDS/latest/index" target="_blank">CDS</a></li>
                 <li><a href="/releases/CDS/latest/GDL2.html" target="_blank">GDL2</a></li>
                 <li><a href="/releases/CDS/latest/GDL.html" target="_blank">GDL</a></li>
-                <li><a href="/components/CDS/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECCDS?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
-                <li></li>
+                <li><a href="/components/CDS/open_issues" target="_blank">CDS PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECCDS?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CDS CRs</a></li>
+                <li> </li>
                 <li><a href="/releases/PROC/latest/index" target="_blank">PROC</a></li>
                 <li><a href="/releases/PROC/latest/task_planning.html" target="_blank">Task Planning (TP)</a></li>
                 <li><a href="/releases/PROC/latest/tp_vml.html" target="_blank">TP Visual Modelling<br>Language (TP-VML)</a></li>
                 <li><a href="/releases/PROC/latest/tp_examples.html" target="_blank">TP Examples</a></li>
                 <li><a href="/releases/PROC/latest/decision_language.html" target="_blank">Decision Language</a></li>
-                <li><a href="/components/PROC/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECPROC?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
+                <li><a href="/components/PROC/open_issues" target="_blank">PROC PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECPROC?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">PROC CRs</a></li>
             </ul>
         </li>
 
@@ -80,12 +80,12 @@
                 <li><a href="/releases/RM/latest/data_structures.html" target="_blank">Data Structures</a></li>
                 <li><a href="/releases/RM/latest/data_types.html" target="_blank">Data Types</a></li>
                 <li><a href="/releases/RM/latest/support.html" target="_blank">Support</a></li>
-                <li><a href="/components/RM/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECRM?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
-                <li></li>
+                <li><a href="/components/RM/open_issues" target="_blank">RM PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECRM?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">RM CRs</a></li>
+                <li> </li>
                 <li><a href="/releases/TERM/latest/index" target="_blank">TERM</a></li>
                 <li><a href="/releases/TERM/latest/SupportTerminology.html" target="_blank">openEHR Terminology</a></li>
-                <li><a href="/components/TERM/open_issues" target="_blank">PRs</a></li>
+                <li><a href="/components/TERM/open_issues" target="_blank">TERM PRs</a></li>
                 <li><a href="https://openehr.atlassian.net/projects/SPECTERM?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
             </ul>
         </li>
@@ -94,9 +94,9 @@
             <ul>
                 <li><a href="/releases/QUERY/latest/index" target="_blank">QUERY</a></li>
                 <li><a href="/releases/QUERY/latest/AQL.html" target="_blank">AQL</a></li>
-                <li><a href="/components/QUERY/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECQUERY?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
-                <li></li>
+                <li><a href="/components/QUERY/open_issues" target="_blank">QUERY PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECQUERY?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">QUERY CRs</a></li>
+                <li> </li>
 
                 <li><a href="/releases/AM/latest/index" target="_blank">AM</a></li>
                 <li><a href="/releases/AM/latest/ADL2.html" target="_blank">ADL 2</a></li>
@@ -106,27 +106,27 @@
                 <li><a href="/releases/AM/latest/AOM1.4.html" target="_blank">AOM 1.4</a></li>
                 <li><a href="" target="_blank">OPT 1.4</a></li>
                 <li><a href="/releases/AM/latest/Identification.html" target="_blank">Identification</a></li>
-                <li><a href="/components/AM/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECAM?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
-                <li></li>
+                <li><a href="/components/AM/open_issues" target="_blank">AM PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECAM?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">AM CRs</a></li>
+                <li> </li>
                 <li><a href="/releases/LANG/latest/index" target="_blank">LANG</a></li>
                 <li><a href="/releases/LANG/latest/expression_language.html" target="_blank">Expression Language</a></li>
                 <li><a href="/releases/LANG/latest/bmm.html" target="_blank">BMM</a></li>
                 <li><a href="/releases/LANG/latest/bmm_persistence.html" target="_blank">P_BMM</a></li>
                 <li><a href="/releases/LANG/latest/odin.html" target="_blank">ODIN</a></li>
-                <li><a href="/components/LANG/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECLANG?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
+                <li><a href="/components/LANG/open_issues" target="_blank">LANG PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECLANG?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">LANG CRs</a></li>
             </ul>
         </li>
 
         <li>Specs Foundations
             <ul>
-                <li><p id="BASE" class="component_name"></li>
+                <li><a href="/releases/BASE/latest/index" target="_blank">BASE</a></li>
                 <li><a href="/releases/BASE/latest/resource.html" target="_blank">Resource</a></li>
                 <li><a href="/releases/BASE/latest/base_types.html" target="_blank">Base Types</a></li>
                 <li><a href="/releases/BASE/latest/foundation_types.html" target="_blank">Foundation Types</a></li>
-                <li><a href="/components/BASE/open_issues" target="_blank">PRs</a></li>
-                <li><a href="https://openehr.atlassian.net/projects/SPECBASE?   orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">CRs</a></li>
+                <li><a href="/components/BASE/open_issues" target="_blank">BASE PRs</a></li>
+                <li><a href="https://openehr.atlassian.net/projects/SPECBASE?orderField=RANK&selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=released-unreleased" target="_blank">BASE CRs</a></li>
             </ul>
         </li>
 
