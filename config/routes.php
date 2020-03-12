1527c938d1cd0daaa209fbc013a796453ef939d4
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2020-03-12 23:03:40 +0100

    handling releases related pages via on single Action class

--- config/routes.php
+++ config/routes.php
@@ -11,6 +11,6 @@ return function (App $app) {
     $app->get('/', Action\WorkingBaselineAction::class);
     $app->get('/manifest', Action\WorkingBaselineAction::class . ':manifest');
     $app->get('/latest_releases', Action\ReleasesAction::class);
-    $app->get('/historical_releases', Action\HistoricalReleasesAction::class);
+    $app->get('/historical_releases', Action\ReleasesAction::class . ':historical');
 //    $app->redirect('/from', '/to', 301);
 };
--- src/Action/ReleasesAction.php
+++ src/Action/ReleasesAction.php
@@ -2,15 +2,39 @@
 
 namespace App\Action;
 
+use App\Domain\ComponentService;
+use App\View;
 use Slim\Http\Response;
 use Slim\Http\ServerRequest;
 
 final class ReleasesAction
 {
+    protected $view;
+    protected $componentService;
+
+    public function __construct(View $view, ComponentService $components)
+    {
+        $this->view = $view;
+        $this->componentService = $components;
+    }
+
     public function __invoke(ServerRequest $request, Response $response): Response
     {
         $response->getBody()->write('releases');
 
-        return $response;
+        $data = [
+                'title' => 'Latest Releases',
+                'page' => 'latest_releases',
+            ] + $this->componentService->getComponents();
+        return $this->view->render($response, 'latest_releases.phtml', $data);
+    }
+
+    public function historical(ServerRequest $request, Response $response, array $args): Response
+    {
+        $data = [
+            'title' => 'Historical Releases',
+            'page' => 'historical_releases',
+        ];
+        return $this->view->render($response, 'historical_releases.phtml', $data);
     }
 }
