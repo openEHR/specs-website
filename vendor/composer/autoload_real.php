8eeaba667be094b020c2fce269ee68bddfea30fd
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2020-09-02 10:48:08 +0200

    updating Slim framework to latest version (using composer)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInita1f6c0506eba66933ac1277253470953
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
