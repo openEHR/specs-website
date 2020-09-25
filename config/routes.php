2bea41c5fad5cd80477978358debc37315eada0b
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2020-09-25 13:29:38 +0200

    adding support to browse ITS-BMM component

--- config/routes.php
+++ config/routes.php
@@ -8,7 +8,7 @@ return function (App $app) {
     $app->get('/releases/{release:(?:0.9|0.95|1.0|1.0.1|1.0.2)}', Action\HistoricalReleasesAction::class . ':index');
     $app->get('/releases/{release:(?:0.9|0.95|1.0|1.0.1|1.0.2)}/{asset:.+}', Action\HistoricalReleasesAction::class . ':assets');
     $app->get('/releases/UML/latest/{asset:.*}', Action\UMLViewerAction::class . ':assets');
-    $app->get('/releases/{component:ITS-XML|ITS-JSON}/{release}/components[/[{asset:.+}]]', Action\ITSDirViewerAction::class);
+    $app->get('/releases/{component:ITS-XML|ITS-JSON|ITS-BMM}/{release}/components[/[{asset:.+}]]', Action\ITSDirViewerAction::class);
     $app->get('/releases/{component}/{alias:open_issues|roadmap|history|crs}', Action\RedirectAction::class . ':jira');
     $app->get('/releases/{component}/{release}/{alias:issues|changes}', Action\RedirectAction::class . ':jira');
     $app->get('/releases/{component}[/[{release}[/[index[.html]]]]]', Action\SpecViewerAction::class . ':index');
