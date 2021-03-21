224249eff5426dc662562a23f370ea5007e4d655
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2021-03-21 12:21:22 +0100

    fixing typo

--- config/routes.php
+++ config/routes.php
@@ -8,7 +8,7 @@ return function (App $app) {
     $app->get('/releases/{release:(?:0.9|0.95|1.0|1.0.1|1.0.2)}', Action\HistoricalReleasesAction::class . ':index');
     $app->get('/releases/{release:(?:0.9|0.95|1.0|1.0.1|1.0.2)}/{asset:.+}', Action\HistoricalReleasesAction::class . ':assets');
     $app->get('/releases/UML/latest/{asset:.*}', Action\UMLViewerAction::class . ':assets');
-    $app->get('/releases/{component:ITS-XML|ITS-JSON|ITS-BMM}/{release}/components[/[{asset:.+}]]', Action\ITSAction::class . 'dir_viewer');
+    $app->get('/releases/{component:ITS-XML|ITS-JSON|ITS-BMM}/{release}/components[/[{asset:.+}]]', Action\ITSAction::class . ':dir_viewer');
     $app->get('/releases/{component}/{alias:open_issues|roadmap|history|crs}', Action\RedirectAction::class . ':jira');
     $app->get('/releases/{component}/{release}/{alias:issues|changes}', Action\RedirectAction::class . ':jira');
     $app->get('/releases/{component:(?:ITS-REST)}/{release}/{asset:index\.html}', Action\SpecViewerAction::class . ':assets');
