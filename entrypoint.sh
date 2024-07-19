13f8e81c3d7300afb0052db0c003a42952362416
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2024-07-19 22:05:33 +0200

    optimizing entrypoint.sh by disabling repo and releases

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,17 @@
 echo "Installing PHP external libraries"
 composer install --no-dev --no-interaction --no-progress --no-ansi --classmap-authoritative
 
-echo "Cloning source repositories"
-/data/website/scripts/init.sh
-
-echo "Generate original (old) releases"
-/data/website/scripts/spec_populate_original_releases.sh
-
-echo "Generate current releases"
-/data/website/scripts/spec_populate_releases_all.sh
+### notice: the following lines are disabled to make the container restart faster;
+### these commands should be manually invoked when ever needed (e.g. when volumes are destroyed) - no real need to have it in the entrypoint
+#
+#echo "Cloning source repositories"
+#/data/website/scripts/init.sh
+#
+#
+#echo "Generate original (old) releases"
+#/data/website/scripts/spec_populate_original_releases.sh
+#
+#echo "Generate current releases"
+#/data/website/scripts/spec_populate_releases_all.sh
 
 exec "$@"
