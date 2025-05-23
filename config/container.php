6c89ae64968737d69cb35e05c096c88788ed50a5
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2025-05-23 17:51:49 +0200

    adding phpstan and parallel-lint dev tools; fixing code according to phpstan

--- config/container.php
+++ config/container.php
@@ -33,11 +33,11 @@ return [
         return $appContext->environment;
     },
 
-    Configuration::class => static function () {
+    Configuration::class => static function (): Configuration {
         return new Configuration(require __DIR__ . '/settings.php');
     },
 
-    App::class => static function (ContainerInterface $container) {
+    App::class => static function (ContainerInterface $container): App {
         AppFactory::setContainer($container);
         return AppFactory::create();
     },
@@ -50,22 +50,22 @@ return [
     DecoratedResponseFactory::class => autowire(),
     DecoratedServerRequestFactory::class => autowire(),
     DecoratedUriFactory::class => autowire(),
-    ResponseFactoryInterface::class => static function (ContainerInterface $container) {
+    ResponseFactoryInterface::class => static function (ContainerInterface $container): ResponseFactoryInterface {
         $psr17Factory = $container->get(Psr17Factory::class);
         return new DecoratedResponseFactory($psr17Factory, $psr17Factory);
     },
-    ServerRequestFactoryInterface::class => static function (ContainerInterface $container) {
+    ServerRequestFactoryInterface::class => static function (ContainerInterface $container): ServerRequestFactoryInterface {
         $psr17Factory = $container->get(Psr17Factory::class);
         return new DecoratedServerRequestFactory($psr17Factory);
     },
-    StreamFactoryInterface::class => static function (ContainerInterface $container) {
+    StreamFactoryInterface::class => static function (ContainerInterface $container): DecoratedUriFactory {
         $psr17Factory = $container->get(Psr17Factory::class);
         return new DecoratedUriFactory($psr17Factory);
     },
     UploadedFileFactoryInterface::class => DI\get(Psr17Factory::class),
     UriFactoryInterface::class => DI\get(Psr17Factory::class),
 
-    View::class => static function (ContainerInterface $container) {
+    View::class => static function (ContainerInterface $container): View {
         /** @var Context $appContext */
         $appContext = $container->get(Context::class);
         $settings = $container->get(Configuration::class)->view;
@@ -73,7 +73,7 @@ return [
         return new View($appContext->dir . '/templates', $settings, $navbar);
     },
 
-    NavBar::class => static function (ContainerInterface $container) {
+    NavBar::class => static function (ContainerInterface $container): NavBar {
         $settings = $container->get(Configuration::class)->navbar;
         return new NavBar($settings);
     },
